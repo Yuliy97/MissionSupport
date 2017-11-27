fcdb6e667d5c52e98aa698944099a2a3e9b253ea
Fayaz Josufi <fjosufi3@gatech.edu>
2017-11-26 22:01:30 -0500

    added sites functionality

---
+++ bootstrap/js/map.js
@@ -0,0 +1,31 @@
+var map;
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13,
+    mapTypeId: 'roadmap'
+  });
+
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('add').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+      document.getElementById('close').click();
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
