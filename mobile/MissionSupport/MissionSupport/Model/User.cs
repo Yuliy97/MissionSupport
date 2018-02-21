29077fb95be60f9944e4b94925ca89b6e3e58abe
taylor1355 <hearntaylor@gmail.com>
2018-02-21 00:08:32 -0500

    removed unnecessary imports

--- mobile/MissionSupport/MissionSupport/Model/User.cs
+++ mobile/MissionSupport/MissionSupport/Model/User.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MissionSupport.Model
+﻿namespace MissionSupport.Model
 {
     class User
     {
