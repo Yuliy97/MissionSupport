d2154f52b9d026b5d862842c0db9e8bdc7345018
annie <atruong32@gatech.edu>
2018-03-08 19:02:06 -0500

    loginTemp

--- mobile/MissionSupport/MissionSupport/View/Login.xaml.cs
+++ mobile/MissionSupport/MissionSupport/View/Login.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using MissionSupport.Model;
+
 
 namespace MissionSupport.View
 {
@@ -11,5 +13,16 @@ namespace MissionSupport.View
         {
             InitializeComponent();
         }
+
+        //void SignIn() {
+        //    //IDatabase db = new IDatabase();
+        //    //User user = new User(UsernameEntry.Text, PasswordEntry.Text);
+
+        //    if (db.login(UsernameEntry.Text, PasswordEntry.Text)) {
+        //        DisplayAlert("Login", "Login Success", "OK");
+        //    } else {
+        //        DisplayAlert("Login", "Login Fail", "OK");
+        //    }
+        //}
     }
 }
