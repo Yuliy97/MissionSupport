fffee742cd9cc558fdb9efab78817bd25a656c86
Taylor Hearn <hearntaylor@gmail.com>
2018-03-24 17:36:50 -0400

    added api key to get map working on android

--- mobile/MissionSupport/MissionSupport/View/Sites.xaml.cs
+++ mobile/MissionSupport/MissionSupport/View/Sites.xaml.cs
@@ -15,22 +15,7 @@ namespace MissionSupport.View
         {
             InitializeComponent();
 
-            map = new Map(MapSpan.FromCenterAndRadius(
-                new Position(36, 36),
-                Distance.FromMiles(0.5)))
-            {
-                IsShowingUser = true,
-                VerticalOptions = LayoutOptions.FillAndExpand
-
-            };
-
-            pin1 = new Pin
-            {
-                Type = PinType.Place,
-                Position = new Position(40, 32),
-                Label = "Test Pin",
-                Address = "www.google.com"
-            };
+            // TODO: show user location if permission granted
         }
     }
 }
