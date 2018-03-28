762899ee739e634ceb6a636a70db145ccf512cc1
taylor1355 <hearntaylor@gmail.com>
2018-03-28 00:12:58 -0400

    Test implementation of pins + geocoding, updated gitignore

--- mobile/MissionSupport/MissionSupport/View/Sites.xaml.cs
+++ mobile/MissionSupport/MissionSupport/View/Sites.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
@@ -8,14 +9,33 @@ namespace MissionSupport.View
 {
     public partial class Sites : ContentPage
     {
-        private Map map;
-        private Pin pin1;
-
         public Sites()
         {
             InitializeComponent();
 
             // TODO: show user location if permission granted
+        }
+
+        protected override async void OnAppearing()
+        {
+            string address = "Tech Tower, Atlanta, GA 30313";
+            Geocoder geocoder = new Geocoder();
+            var addressMatches = await geocoder.GetPositionsForAddressAsync(address);
+            Position pos = new Position();
+            try {
+                pos = addressMatches.First();
+            } catch (InvalidOperationException) {
+                await DisplayAlert("Error", "That address could not be found", "OK");
+                return;
+            }
+
+            Pin testPin = new Pin() {
+                Position = pos,
+                Label = "TEST"
+            };
+            
+            SitesMap.Pins.Add(testPin);
+            SitesMap.MoveToRegion(MapSpan.FromCenterAndRadius(pos, new Distance(1000)));
         }
     }
 }
