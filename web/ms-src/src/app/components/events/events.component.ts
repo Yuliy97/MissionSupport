e3ccaa26f30caec49ea9bcb0ecfc5ebb4513309b
Fayaz Josufi <fjosufi3@gatech.edu>
2018-04-22 21:27:45 -0400

    footer link update

--- web/ms-src/src/app/components/events/events.component.ts
+++ web/ms-src/src/app/components/events/events.component.ts
@@ -2,12 +2,20 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { CalendarComponent } from 'ng-fullcalendar';
 import { Options } from 'fullcalendar';
 
+var _events = [{title: 'Mission Support Capstone', start: '2018-04-23', end: '2018-04-24'}];
+
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
+
+  event_name: string;
+  attendees: string;
+  event_description: string;
+  start_date: string;
+  end_date: string;
 
   calendarOptions: Options;
   @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
@@ -21,14 +29,32 @@ export class EventsComponent implements OnInit {
           center: 'title',
           right: 'month,agendaWeek,agendaDay,listMonth'
         },
-        events: [{
-          title: 'Mission Support Capstone',
-          start: '2018-04-23',
-          end: '2018-04-24'
-        }],
+        events: _events,
         eventColor: '#378006',
-        eventTextColor: 'white'
+        eventTextColor: 'white',
+        eventClick: function(event) {
+          console.log("HELLO");
+          alert('Event ' + event.title);
+        }
       };
+  }
+
+  // format(str) {
+  //   if (str) {
+  //     var result = str.substring(6, 10) + "-" + str.substring(0, 2) + "-" + str.substring(3, 5)
+  //     return result;
+  //   } else {
+  //     return "2018-04-01";
+  //   }
+  // }
+
+  on_add() {
+    const event = {
+      title: this.event_name,
+      start: "2018-04-25",
+      end: "2018-04-26"
+    }
+    this.ucCalendar.fullCalendar('renderEvent', event);
   }
 
 }
